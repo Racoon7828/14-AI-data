3c3000322389d8dc419d7505cb36e5e43f83d4a6
racoon7828 <racoon7828@naver.com>
2026-04-02 21:21:15 +0900

    0402 강의자료

--- 05_플라스크/app.py
+++ 05_플라스크/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+# 플라스크
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
